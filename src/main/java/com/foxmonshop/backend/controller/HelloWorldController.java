87c9b7effef7c1b53730c5fc7b0f33fbb0ce3a08
foxmon <jjockrod@naver.com>
2024-01-17 22:13:37 +0900

    init

---
+++ src/main/java/com/foxmonshop/backend/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.foxmonshop.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping("")
+    public ResponseEntity<String> getHelloWorld() {
+        return ResponseEntity.ok().body("Hello");
+    }
+}
